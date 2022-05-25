56c1d795d0c9068ccd21e7b4b360783546c316f3
sekkun foo <sekkun.luke@gmail.com>
2022-05-25 14:33:53 +0800

    added all functions

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,20 @@ Rails.application.routes.draw do
   # This action should fetch all tasks, and a view should loop over these to display them.
   get 'tasks', to: 'tasks#index'
 
+  # As a user, I can add task
+  # Get the form, then create the post
+  get "tasks/new", to: "tasks#new"
+
   # As a user, I can view task details
   # show, by id
   get 'tasks/:id', to: 'tasks#show', as: :task
+  post "tasks", to: "tasks#create"
+
+  # As a user, I can edit task
+  # Get and patch
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # Delete
+  delete "tasks/:id", to: "tasks#destroy"
 end
