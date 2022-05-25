6c1ee54d855de72cf9c5c0e87c2c79c85599c25a
sekkun foo <sekkun.luke@gmail.com>
2022-05-25 13:47:25 +0800

    addded index and show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,17 @@
 class TasksController < ApplicationController
+  # before_action :select_task, only: [:show]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def select_task
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # As a user I can list tasks
   # Add a new route to list task
+  # add a controller action and its view
+  # This action should fetch all tasks, and a view should loop over these to display them.
+  get 'tasks', to: 'tasks#index'
+
+  # As a user, I can view task details
+  # show, by id
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
